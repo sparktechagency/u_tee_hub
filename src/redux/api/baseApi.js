354a65913b3bf639fbd0e2b058ffb339e2b888f3
rayhan108 <raihanshorker56@gmail.com>
2025-10-05 17:20:02 +0600

    add bearer token

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -7,7 +7,7 @@ const baseQuery = fetchBaseQuery({
     prepareHeaders: (headers, { getState }) => {
       const token = (getState()).auth.token;
       if (token) {
-        headers.set("authorization", `${token}`);
+        headers.set("authorization", `Bearer ${token}`);
       }
       return headers;
     },
