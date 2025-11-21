c0ea1c0cd24a8567f1e30b12d7ca9b52045efed2
rayhan108 <raihanshorker56@gmail.com>
2025-11-21 10:08:00 +0600

    modify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'redux-vendor': ['@reduxjs/toolkit', 'react-redux', 'redux-persist'],
+          'ui-vendor': ['antd', '@ant-design/icons'],
+          'chart-vendor': ['apexcharts', 'react-apexcharts', 'recharts'],
+          'editor-vendor': ['react-quill'],
+          'icons-vendor': ['react-icons'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Increase limit if needed
+  }
 })
