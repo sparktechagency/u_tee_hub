ee0834c8b4bc4f81172afc2366722e2e0b72031f
rayhan108 <raihanshorker56@gmail.com>
2025-11-28 16:20:29 +0600

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// 
+
 export default defineConfig({
   plugins: [react()],
   build: {
-    reportCompressedSize: false,
+
     rollupOptions: {
       output: {
+
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+        
+  
         manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'redux-vendor': ['@reduxjs/toolkit', 'react-redux', 'redux-persist'],
-          'antd-core': ['antd'],
-          'apexcharts': ['apexcharts'],
-          'react-apexcharts': ['react-apexcharts'],
-          'recharts': ['recharts'],
-          'editor-vendor': ['react-quill'],
-          'icons-vendor': ['react-icons'],
-          'utils': ['dayjs', 'jwt-decode', 'localforage'],
-          'form-utils': ['react-hook-form', 'react-toastify', 'sweetalert2']
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          antd: ['antd'],
         }
       }
     },
-    chunkSizeWarningLimit: 1600
+    // Warning limit বাড়াও
+    chunkSizeWarningLimit: 2000,
   }
 })
